dc69b65a4e230883c2dd1283916817e01c3aec98
Matheus G. Dias <matheusgiga123@gmail.com>
2025-06-12 15:01:22 -0300

    feat: integrate Tailwind CSS and PostCSS into the project

--- rotion/src/renderer/src/App.tsx
+++ rotion/src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 export function App(): React.JSX.Element {
   return (
     <div>
-      <h1>Welcome to Rotions!</h1>
+      <h1 className="text-violet-400 text-4xl">Welcome to Rotions!</h1>
       <p>This is a simple React application.</p>
     </div>
   );
